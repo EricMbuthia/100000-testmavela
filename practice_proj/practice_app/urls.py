7680a25e3ef8470604653546b5850c454ed36a9d
unknown <ericmbuthia11@gmail.com>
2023-11-07 12:01:19 +0300

    added url

--- practice_proj/practice_app/urls.py
+++ practice_proj/practice_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from practice_app import views
 
 urlpatterns = [
-path('home/', views.home, name='home')
+path('', views.home, name='home'),
+
+path('home/', views.home, name='home'),
 
 ]
